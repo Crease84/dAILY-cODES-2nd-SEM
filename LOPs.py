ba61fc880cdc287177c72765944b3f10214d06aa
Crease84 <91694954+Crease84@users.noreply.github.com>
2022-01-21 13:53:29 -0700

    Create LOPs.py

---
+++ LOPs.py
@@ -0,0 +1,25 @@
+import random
+
+for x in range(1,13):
+    print(x)
+    
+for x in range(8,41,2):
+    print(x)
+    
+for z in range(20,-21,-5):
+    print(z)
+    
+print("HOW MANNY")
+x = int(input())
+w=0
+
+while(w != x):
+    print("Im gonna gift you")
+    w+=1
+    
+print()
+
+while(x != 7):
+    x = random.randrange(1,30)
+    print(x)
+    
